bb76902bd1b912d58eb42a4e8fbbd422c576bda3
05Nidhi <budevisharma18aug@gmail.com>
2023-07-20 19:28:00 +0530

    final commit

--- app/admin/programs.rb
+++ app/admin/programs.rb
@@ -14,7 +14,38 @@ ActiveAdmin.register Program do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  def self.ransackable_attributes(auth_object = nil)
-    ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :status
+    column :video
+    column :user_id
+    column :category_id
+    column :created_at
+    actions
   end
+
+  filter :name
+  filter :status
+  filter :video
+  filter :user_id
+  filter :category_id
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :status
+      f.input :video
+      f.input :user_id
+      f.input :category_id
+      f.input :created_at
+    end
+    f.actions
+  end
+
+  # def self.ransackable_attributes(auth_object = nil)
+  #   ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+  # end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -10,14 +10,14 @@ class Program < ApplicationRecord
   validates :status, presence:true, inclusion: {in: %w(active inactive), message: "%{value} is not valid"}
 
   def self.ransackable_attributes(auth_object = nil)
-    ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+    ["blob_id", "created_at", "id","status", "name", "record_id", "record_type"]
   end
 
   def self.ransackable_associations(auth_object = nil)
     ["category", "enrolls", "user", "video_attachment", "video_blob"]
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
-  end
+  # def self.ransackable_attributes(auth_object = nil)
+  #   ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+  # end
 end
